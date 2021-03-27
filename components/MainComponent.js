51cdf319332d769752214936c4e84597d3b7d1a8
Laura Harston <lauraharston@repeteurwifi-599d86.home>
2021-03-27 15:06:09 +0100

    Network Info

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -14,6 +14,8 @@ import {
   Text,
   ScrollView,
   Image,
+  Alert,
+  ToastAndroid
 } from "react-native";
 import { createStackNavigator } from "react-navigation-stack";
 import { createDrawerNavigator, DrawerItems } from "react-navigation-drawer";
@@ -27,6 +29,9 @@ import {
   fetchPromotions,
   fetchPartners,
 } from "../redux/ActionCreators";
+
+import NetInfo from '@react-native-community/netinfo';
+
 
 const mapDispatchToProps = {
   fetchCampsites,
@@ -329,7 +334,45 @@ class Main extends Component {
     this.props.fetchComments();
     this.props.fetchPartners();
     this.props.fetchPromotions();
+
+    NetInfo.fetch().then(connectionInfo => {
+      (Platform.OS === 'ios')
+          ? Alert.alert('Initial Network Connectivity Type:', connectionInfo.type)
+          : ToastAndroid.show('Initial Network Connectivity Type: ' +
+              connectionInfo.type, ToastAndroid.LONG);
+  });
+
+    this.unsuscribeNetInfo = NetInfo.addEventListener(connectionInfo => {
+      this.handleConnectivityChange(connectionInfo)
+    })
   }
+
+  componentWillUnmount() {
+    this.unsuscribeNetInfo()
+  }
+
+  handleConnectivityChange = connectionInfo => {
+    let connectionMsg = 'You are now connected to an active network'
+    switch (connectionInfo.type) {
+      case 'none':
+          connectionMsg = 'No network connection is active';
+          break;
+      case 'unknown':
+          connectionMsg = 'The network connection state is unknown';
+          break;
+      case 'cellular':
+          connectionMsg = 'You are now connected to a cellular network';
+          break;
+      case 'wifi':
+          connectionMsg = 'You are now connected to a Wifi network';
+          break;     
+    }
+    (Platform.OS === 'ios')
+      ? Alert.alert('Connection change', connectionMsg)
+      : ToastAndroid.show(connectionMsg, ToastAndroid.LONG)
+  }
+
+
   render() {
     return (
       <View
